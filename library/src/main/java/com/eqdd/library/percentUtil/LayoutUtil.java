a309ce14a311600b62b9319afd594f751eb31a2f
lvzhihao <1030753080@qq.com>
2017-05-12 11:54:47 +0800

    5.12 增加maxHeight百分比适配 增加maxWidth百分比适配

--- library/src/main/java/com/eqdd/library/percentUtil/LayoutUtil.java
+++ library/src/main/java/com/eqdd/library/percentUtil/LayoutUtil.java
@@ -14,26 +14,27 @@ public class LayoutUtil {
 
     @BindingAdapter({"android:layout_margin"})
     public static void setMargin(View view, int margin) {
-        margin=(margin*WindowUtil.csw/ WindowUtil.width);
-        if(view != null) {
-            setMargin(view, margin,margin,margin, margin);
+        margin = (margin * WindowUtil.csw / WindowUtil.width);
+        if (view != null) {
+            setMargin(view, margin, margin, margin, margin);
         }
 
     }
+
     @BindingAdapter({"android:padding"})
     public static void setPadding(View view, int padding) {
-        padding=(padding*WindowUtil.csw/ WindowUtil.width);
-        if(view != null) {
-            view.setPadding(padding,padding,padding, padding);
+        padding = (padding * WindowUtil.csw / WindowUtil.width);
+        if (view != null) {
+            view.setPadding(padding, padding, padding, padding);
         }
 
     }
 
     public static void setMargin(View view, int left, int right, int top, int bottom) {
-        if(view != null) {
+        if (view != null) {
             ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
-            if(layoutParams != null && layoutParams instanceof ViewGroup.MarginLayoutParams) {
-                ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams)layoutParams;
+            if (layoutParams != null && layoutParams instanceof ViewGroup.MarginLayoutParams) {
+                ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams) layoutParams;
                 marginLayoutParams.leftMargin = left;
                 marginLayoutParams.topMargin = top;
                 marginLayoutParams.rightMargin = right;
@@ -44,18 +45,19 @@ public class LayoutUtil {
 
         }
     }
+
     @BindingAdapter({"android:textSize"})
     public static void setTextSize(TextView view, int size) {
-        size=(size*WindowUtil.csw/ WindowUtil.width);
-        if(view != null) {
-            view.setTextSize(TypedValue.COMPLEX_UNIT_PX,size);
+        size = (size * WindowUtil.csw / WindowUtil.width);
+        if (view != null) {
+            view.setTextSize(TypedValue.COMPLEX_UNIT_PX, size);
         }
     }
 
 
     @BindingAdapter({"size"})
     public static void setSize(View view, int[] size) {
-        if(view != null && size != null && size.length == 2) {
+        if (view != null && size != null && size.length == 2) {
             ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
             layoutParams.width = size[0];
             layoutParams.height = size[1];
@@ -66,8 +68,8 @@ public class LayoutUtil {
 
     @BindingAdapter({"layout_width"})
     public static void setLayoutWidth(View view, int width) {
-        width=width*WindowUtil.csw/ WindowUtil.width;
-        if(view != null) {
+        width = width * WindowUtil.csw / WindowUtil.width;
+        if (view != null) {
             ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
             layoutParams.width = width;
             view.setLayoutParams(layoutParams);
@@ -77,8 +79,8 @@ public class LayoutUtil {
 
     @BindingAdapter({"layout_height"})
     public static void setLayoutHeight(View view, int height) {
-        height=height*WindowUtil.csw/ WindowUtil.width;
-        if(view != null) {
+        height = height * WindowUtil.csw / WindowUtil.width;
+        if (view != null) {
             ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
             layoutParams.height = height;
             view.setLayoutParams(layoutParams);
@@ -88,46 +90,44 @@ public class LayoutUtil {
 
     @BindingAdapter({"android:paddingLeft"})
     public static void setPaddingLeft(View view, int padding) {
-        padding=(padding*WindowUtil.csw/ WindowUtil.width);
-        if (view!=null) {
-            view.setPadding(padding, view.getPaddingTop(),view.getPaddingRight() , view.getPaddingBottom());
+        padding = (padding * WindowUtil.csw / WindowUtil.width);
+        if (view != null) {
+            view.setPadding(padding, view.getPaddingTop(), view.getPaddingRight(), view.getPaddingBottom());
         }
     }
 
     @BindingAdapter({"android:paddingRight"})
     public static void setPaddingRight(View view, int padding) {
-        padding=(padding*WindowUtil.csw/ WindowUtil.width);
-        if (view!=null) {
-            view.setPadding(view.getPaddingLeft(),view.getPaddingTop(), padding, view.getPaddingBottom());
+        padding = (padding * WindowUtil.csw / WindowUtil.width);
+        if (view != null) {
+            view.setPadding(view.getPaddingLeft(), view.getPaddingTop(), padding, view.getPaddingBottom());
         }
     }
 
     @BindingAdapter({"android:paddingTop"})
     public static void setPaddingTop(View view, int padding) {
-        padding=(padding*WindowUtil.csw/ WindowUtil.width);
-        if (view!=null) {
-            view.setPadding(view.getPaddingLeft(),padding, view.getPaddingRight() , view.getPaddingBottom());
+        padding = (padding * WindowUtil.csw / WindowUtil.width);
+        if (view != null) {
+            view.setPadding(view.getPaddingLeft(), padding, view.getPaddingRight(), view.getPaddingBottom());
         }
     }
 
     @BindingAdapter({"android:paddingBottom"})
     public static void setPaddingBottom(View view, int padding) {
-        padding=(padding*WindowUtil.csw/ WindowUtil.width);
-        if (view!=null) {
-            view.setPadding(view.getPaddingLeft(),view.getPaddingTop(), view.getPaddingRight() , padding);
+        padding = (padding * WindowUtil.csw / WindowUtil.width);
+        if (view != null) {
+            view.setPadding(view.getPaddingLeft(), view.getPaddingTop(), view.getPaddingRight(), padding);
         }
     }
 
 
-
-
     @BindingAdapter({"android:layout_marginLeft"})
     public static void setMarginLeft(View view, int margin) {
-        margin=(margin*WindowUtil.csw/ WindowUtil.width);
-        if(view != null) {
+        margin = (margin * WindowUtil.csw / WindowUtil.width);
+        if (view != null) {
             ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
-            if(layoutParams != null && layoutParams instanceof ViewGroup.MarginLayoutParams) {
-                ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams)layoutParams;
+            if (layoutParams != null && layoutParams instanceof ViewGroup.MarginLayoutParams) {
+                ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams) layoutParams;
                 marginLayoutParams.leftMargin = margin;
                 view.setLayoutParams(marginLayoutParams);
             }
@@ -137,11 +137,11 @@ public class LayoutUtil {
 
     @BindingAdapter({"android:layout_marginRight"})
     public static void setMarginRight(View view, int margin) {
-        margin=(margin*WindowUtil.csw/ WindowUtil.width);
-        if(view != null) {
+        margin = (margin * WindowUtil.csw / WindowUtil.width);
+        if (view != null) {
             ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
-            if(layoutParams != null && layoutParams instanceof ViewGroup.MarginLayoutParams) {
-                ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams)layoutParams;
+            if (layoutParams != null && layoutParams instanceof ViewGroup.MarginLayoutParams) {
+                ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams) layoutParams;
                 marginLayoutParams.rightMargin = margin;
                 view.setLayoutParams(marginLayoutParams);
             }
@@ -151,11 +151,11 @@ public class LayoutUtil {
 
     @BindingAdapter({"android:layout_marginTop"})
     public static void setMarginTop(View view, int margin) {
-        margin=(margin*WindowUtil.csw/ WindowUtil.width);
-        if(view != null) {
+        margin = (margin * WindowUtil.csw / WindowUtil.width);
+        if (view != null) {
             ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
-            if(layoutParams != null && layoutParams instanceof ViewGroup.MarginLayoutParams) {
-                ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams)layoutParams;
+            if (layoutParams != null && layoutParams instanceof ViewGroup.MarginLayoutParams) {
+                ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams) layoutParams;
                 marginLayoutParams.topMargin = margin;
                 view.setLayoutParams(marginLayoutParams);
             }
@@ -165,15 +165,30 @@ public class LayoutUtil {
 
     @BindingAdapter({"android:layout_marginBottom"})
     public static void setMarginBottom(View view, int margin) {
-        margin=(margin*WindowUtil.csw/ WindowUtil.width);
-        if(view != null) {
+        margin = (margin * WindowUtil.csw / WindowUtil.width);
+        if (view != null) {
             ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
-            if(layoutParams != null && layoutParams instanceof ViewGroup.MarginLayoutParams) {
-                ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams)layoutParams;
+            if (layoutParams != null && layoutParams instanceof ViewGroup.MarginLayoutParams) {
+                ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams) layoutParams;
                 marginLayoutParams.bottomMargin = margin;
                 view.setLayoutParams(marginLayoutParams);
             }
         }
 
+    }
+
+    @BindingAdapter({"android:maxWidth"})
+    public static void setMaxWidth(View view, int maxWidth) {
+        maxWidth = (maxWidth * WindowUtil.csw / WindowUtil.width);
+        if (view instanceof TextView) {
+            ((TextView) view).setMaxWidth(maxWidth);
+        }
+    }
+    @BindingAdapter({"android:maxHeight"})
+    public static void setMaxHeight(View view, int maxHeight) {
+        maxHeight = (maxHeight * WindowUtil.csw / WindowUtil.width);
+        if (view instanceof TextView) {
+            ((TextView) view).setMaxHeight(maxHeight);
+        }
     }
 }
