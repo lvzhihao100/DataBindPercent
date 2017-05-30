24da86e1d21bf3bd7329aed14ae6a3c54970fcd5
lvzhihao <1030753080@qq.com>
2017-05-30 16:22:40 +0800

    6.01 修改库的包名

--- app/src/main/java/com/eqdd/databindpercent/MainActivity.java
+++ app/src/main/java/com/eqdd/databindpercent/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        com.eqdd.databindpercent.MainActivityCustom da=  DataBindingUtil.setContentView(this,R.layout.activity_main);
+        com.eqdd.databindpercent.MainActivityCustom da = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
